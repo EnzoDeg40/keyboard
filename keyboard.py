ebb2a4f7263eea29683a5d9d6817333886517a3c
Enzo Degraeve <enzodeg40@gmail.com>
2024-12-05 20:58:51 +0100

    Add key range

--- keyboard.py
+++ keyboard.py
@@ -2,17 +2,20 @@ from machine import Pin
 import utime
 
 matrix_keys = [
-    ['1', '2'],
-    ['3', '4']
+    ['1', '2', '3', '4', '5'],
+    ['6', '7', '8', '9', '10'],
+    ['11', '12', '13', '14', '15'],
+    ['16', '17', '18', '19', '20'],
+    ['21', '22', '23', '24', '25']
 ]
 
-keypad_cols = [25, 24]
-keypad_rows = [0, 1]
+keypad_cols = [25, 24, 23, 22, 21]
+keypad_rows = [0, 1, 3, 4, 5]
 
 col_pins = []
 row_pins = []
 
-for x in range(0, 2):
+for x in range(0, 5):
     row_pins.append(Pin(keypad_rows[x], Pin.OUT))
     row_pins[x].value(1)
     col_pins.append(Pin(keypad_cols[x], Pin.IN, Pin.PULL_DOWN))
@@ -21,8 +24,8 @@ for x in range(0, 2):
 print("Please press a key")
 
 def scankeys():
-    for row in range(2):
-        for col in range(2):
+    for row in range(5):
+        for col in range(5):
             row_pins[row].high()
             key = None
 
